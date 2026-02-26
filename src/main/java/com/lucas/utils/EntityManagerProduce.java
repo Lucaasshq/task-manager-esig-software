2490d5b79857d151d2d02de57165fd77380b8f67
lucaasshq <lucashenriquebarros2016@gmail.com>
2026-02-26 17:27:33 -0300

    finalizando

--- src/main/java/com/lucas/utils/EntityManagerProduce.java
+++ src/main/java/com/lucas/utils/EntityManagerProduce.java
@@ -17,27 +17,8 @@ public class EntityManagerProduce {
 	private EntityManagerFactory emf;
 
 	public EntityManagerProduce() {
-		Map<String, String> props = new HashMap<>();
-	    
-	    // Pegamos as variáveis brutas do Railway
-	    String host = System.getenv("PGHOST");
-	    String port = System.getenv("PGPORT");
-	    String db   = System.getenv("PGDATABASE");
-	    String user = System.getenv("PGUSER");
-	    String pass = System.getenv("PGPASSWORD");
 
-	    // Montamos a URL JDBC manualmente no Java
-	    String url = String.format("jdbc:postgresql://%s:%s/%s", host, port, db);
-
-	    props.put("javax.persistence.jdbc.url", url);
-	    props.put("javax.persistence.jdbc.user", user);
-	    props.put("javax.persistence.jdbc.password", pass);
-	    props.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
-
-	    // Garante que o Hibernate use o dialeto correto
-	    props.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-
-		this.emf = Persistence.createEntityManagerFactory("TarefasPU", props);
+		this.emf = Persistence.createEntityManagerFactory("TarefasPU");
 	}
 	
 	@Produces
